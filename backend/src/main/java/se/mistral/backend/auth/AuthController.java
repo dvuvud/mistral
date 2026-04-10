49fd1212361da58b20a5fafb6a5259f24dbab7d7
dvuvud <carldavid.buhrman@gmail.com>
2026-04-10 20:44:02 +0200

    implement simple authcontroller with exposing a registration and a login endpoint

---
+++ backend/src/main/java/se/mistral/backend/auth/AuthController.java
@@ -0,0 +1,25 @@
+package se.mistral.backend.auth;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import se.mistral.backend.auth.dto.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;      // set by lombok
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
