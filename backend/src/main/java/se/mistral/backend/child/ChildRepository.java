c9730cfea78fa8f6d8f3a3b9968cda59fb673bd1
MuschBenne <sjostrand.benjamin@gmail.com>
2026-04-14 16:50:39 +0200

    add child feature

---
+++ backend/src/main/java/se/mistral/backend/child/ChildRepository.java
@@ -0,0 +1,10 @@
+package se.mistral.backend.child;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChildRepository extends JpaRepository<Child, Long> {
+    // findAll() finns redan inbyggt, inget behövs läggas till
+}
+
