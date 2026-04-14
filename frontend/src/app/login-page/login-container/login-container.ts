2ff04d7b495b0381a7f018f49eb4c7c097a7f1e1
= <=>
2026-04-14 16:13:32 +0200

    Added some comments for clarity

--- frontend/src/app/login-page/login-container/login-container.ts
+++ frontend/src/app/login-page/login-container/login-container.ts
@@ -8,18 +8,30 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-login-container',
   imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
-  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './login-container.html',
   styleUrl: './login-container.scss',
 })
 export class LoginContainer {
+  /*
+  Model är en speciell typ av input som tillåter 2-way-binding.
+  Det innebär alltså att föräldern kan skicka en signal till barnet,
+  och värdet på signalen propageras tillbaka till föräldern när barnet ändrar det.
+  */
   email = model.required<string>();
   password = model.required<string>();
+
+  /*
+  Detta är lite data för password-fältet
+  */
   hide = signal(true);
   clickEvent(event: MouseEvent) {
     this.hide.set(!this.hide());
     event.stopPropagation();
   }
+
+  /*
+  Här definierar vi ett event som vi vill att föräldern ska kunna ta emot.
+  */
   @Output() loginSubmitEvent = new EventEmitter();
   submitLogin() {
     this.loginSubmitEvent.emit();
