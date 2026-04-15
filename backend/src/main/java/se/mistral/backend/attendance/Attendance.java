e041b03ab030bd2ba357f5b43c3eb0aa525af783
Alec <alec@andin.se>
2026-04-15 16:31:59 +0200

    add Attendance class fields.

--- backend/src/main/java/se/mistral/backend/attendance/Attendance.java
+++ backend/src/main/java/se/mistral/backend/attendance/Attendance.java
@@ -1,5 +1,23 @@
 package se.mistral.backend.attendance;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "attendance", uniqueConstraints = {@UniqueConstraint(name = "UniqueChildIdAndDate", columnNames = {"child_id", "date"})})
 public class Attendance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private long child_id;
+    private Date date;
+    private boolean present;
+    @Version
+    private Version version;
 
 }
