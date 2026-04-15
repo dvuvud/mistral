b582696654ef2e680731f9c6aa9297f2fe22d902
dvuvud <carldavid.buhrman@gmail.com>
2026-04-15 21:04:40 +0200

    remove redundant notblank annotation

--- backend/src/main/java/se/mistral/backend/attendance/dto/AttendanceRequest.java
+++ backend/src/main/java/se/mistral/backend/attendance/dto/AttendanceRequest.java
@@ -2,7 +2,6 @@ package se.mistral.backend.attendance.dto;
 
 import java.time.LocalDate;
 import java.util.Optional;
-import jakarta.validation.constraints.NotBlank;
 
-public record AttendanceRequest(@NotBlank Long childId, Optional<LocalDate> date, @NotBlank Boolean present) {
+public record AttendanceRequest(Long childId, Optional<LocalDate> date, Boolean present) {
 }
