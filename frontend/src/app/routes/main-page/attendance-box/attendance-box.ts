64fe4dd5af980179e91b054a7c3bbb3f44001875
Axel Segerholm <Axel.segerholm@gmail.com>
2026-04-15 18:53:00 +0200

    nu finns fungerande logik för checkbox

--- frontend/src/app/routes/main-page/attendance-box/attendance-box.ts
+++ frontend/src/app/routes/main-page/attendance-box/attendance-box.ts
@@ -1,5 +1,7 @@
+import { C } from '@angular/cdk/keycodes';
 import { Component, model } from '@angular/core';
-import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCheckboxChange, MatCheckboxModule } from '@angular/material/checkbox';
+
 @Component({
   selector: 'app-attendance-box',
   imports: [MatCheckboxModule],
@@ -8,4 +10,27 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 })
 export class AttendanceBox {
     childSignal = model.required<string>();
+    isChecked = false;
+    errorMessage = '';
+
+
+    onCheckBox(event: MatCheckboxChange) {
+          if (event.checked) {
+              
+            const isValid = false; //TODO: byt ut mot registreringscheck
+
+            if(isValid) {
+              this.isChecked = true; 
+            } else {
+              event.source.checked = false;
+              this.isChecked = false;
+              this.errorMessage = 'Misslyckades! Detta barn är redan registrerad som närvarande.'     
+              
+              setTimeout(() => {
+                this.errorMessage = '';
+              }, 50)
+            }
+
+          }
+      }
 }
