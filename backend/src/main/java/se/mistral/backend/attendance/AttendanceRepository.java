4bef724e6ad88d5759f77760c588ca97fef1bf8b
dvuvud <carldavid.buhrman@gmail.com>
2026-04-15 20:57:01 +0200

    fix typos and make small style changes

--- backend/src/main/java/se/mistral/backend/attendance/AttendanceRepository.java
+++ backend/src/main/java/se/mistral/backend/attendance/AttendanceRepository.java
@@ -1,9 +1,7 @@
 package se.mistral.backend.attendance;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import se.mistral.backend.child.Child;
 
 import java.util.Date;
 import java.util.Optional;
