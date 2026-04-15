4bcbbdf5be9da3c0454702e5396d079c4e2ade94
Alec <alec@andin.se>
2026-04-15 20:34:52 +0200

    add Attendance controller

--- backend/src/main/java/se/mistral/backend/attendance/AttendanceController.java
+++ backend/src/main/java/se/mistral/backend/attendance/AttendanceController.java
@@ -1,4 +1,26 @@
 package se.mistral.backend.attendance;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import se.mistral.backend.attendance.dto.AttendanceDto;
+import se.mistral.backend.attendance.dto.AttendanceDtoList;
+import se.mistral.backend.attendance.dto.AttendanceRequest;
+
+@RestController
+@RequestMapping("/api/attendance")
+@RequiredArgsConstructor
 public class AttendanceController {
+
+    private final AttendanceService attendanceService;
+
+    @PostMapping("/")
+    public ResponseEntity<AttendanceDto> updateAttendance(@RequestBody AttendanceRequest request) {
+        return ResponseEntity.ok(attendanceService.updateAttendance(request));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<AttendanceDtoList> getAttendances(@RequestBody AttendanceRequest request) {
+        return ResponseEntity.ok(attendanceService.getAttendances(request));
+    }
 }
