73a52280d5a23cfedff0375cc5a8ddd62ed750c1
Imran Abdihakim <imuu.dv@outlook.com>
2026-04-15 23:47:10 +0200

    add main container and logout button

--- frontend/src/app/routes/main-page/main-page.ts
+++ frontend/src/app/routes/main-page/main-page.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MainPanel } from './main-panel/main-panel';
+import { ChildList } from './main-child-list/main-child-list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
+import { MatCard } from '@angular/material/card';
 
 @Component({
   selector: 'app-main-page',
-  imports: [MainPanel],
+  imports: [
+    MatToolbarModule,
+    MainPanel,
+    MatSidenavModule,
+    MatButtonModule,
+  ],
+
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss',
 })
 export class MainPage {
 
+  constructor(private router: Router) { }
+  logout() {
+    this.router.navigateByUrl("/");
+  }
 }
--- frontend/src/app/routes/main-page/main-panel/main-panel.ts
+++ frontend/src/app/routes/main-page/main-panel/main-panel.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MatCard } from '@angular/material/card';
 import { ChildList } from '../main-child-list/main-child-list';
+
+/*
+import { ChildList } from '../main-child-list/main-child-list';
+*/
 
 @Component({
   selector: 'main-panel',
-  imports: [ChildList],
+  imports: [MatCard, ChildList],
   templateUrl: './main-panel.html',
   styleUrl: './main-panel.scss',
 })
