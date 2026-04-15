af61c35c59037b24a2bb4e0eb3240469043ba485
Alec <alec@andin.se>
2026-04-15 20:36:15 +0200

    add Attendance Service

--- backend/src/main/java/se/mistral/backend/attendance/AttendanceService.java
+++ backend/src/main/java/se/mistral/backend/attendance/AttendanceService.java
@@ -1,4 +1,44 @@
 package se.mistral.backend.attendance;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import se.mistral.backend.attendance.dto.AttendanceDto;
+import se.mistral.backend.attendance.dto.AttendanceDtoList;
+import se.mistral.backend.attendance.dto.AttendanceRequest;
+import se.mistral.backend.child.Child;
+import se.mistral.backend.child.ChildRepository;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class AttendanceService {
+
+    private final AttendanceRepository attendanceRepository;
+    private final ChildRepository childRepository;
+
+
+    private AttendanceDto entitiyToDto(Attendance attendance) {
+        return new AttendanceDto(attendance.getId(),
+                attendance.getChild().getId(),
+                attendance.getDate(),
+                attendance.getPresent());
+    }
+
+
+    public AttendanceDtoList getAttendances(AttendanceRequest request) {
+        List<AttendanceDto> attendanceDtoList = attendanceRepository.findByChildId(request.childId()).stream().map(this::entitiyToDto).toList();
+        return new AttendanceDtoList(attendanceDtoList, attendanceDtoList.size());
+    }
+
+    public AttendanceDto updateAttendance(AttendanceRequest request) {
+        Date date = request.date().orElseThrow(()-> new RuntimeException("No date given"));
+        Attendance attendance = attendanceRepository.findByChildIdAndDate(request.childId(), date).orElseThrow(()-> new RuntimeException("Attendance not found"));
+        attendance.setPresent(request.present());
+        return entitiyToDto(attendance);
+
+    }
+
 }
+
