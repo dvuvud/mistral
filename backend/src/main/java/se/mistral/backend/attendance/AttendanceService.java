fb74f466ecbb0e06745f86873135cd5f22ea8672
dvuvud <carldavid.buhrman@gmail.com>
2026-04-17 14:16:37 +0200

    simplify attendance service

--- backend/src/main/java/se/mistral/backend/attendance/AttendanceService.java
+++ backend/src/main/java/se/mistral/backend/attendance/AttendanceService.java
@@ -2,15 +2,13 @@ package se.mistral.backend.attendance;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import se.mistral.backend.attendance.dto.AttendanceDto;
-import se.mistral.backend.attendance.dto.AttendanceDtoList;
-import se.mistral.backend.attendance.dto.AttendanceListItem;
-import se.mistral.backend.attendance.dto.AttendanceRequest;
-import se.mistral.backend.attendance.dto.AttendanceFetchRequest;
-import se.mistral.backend.attendance.dto.AttendancesRequest;
-import se.mistral.backend.child.ChildRepository;
-
 import se.mistral.backend.exception.NotFoundException;
+
+import se.mistral.backend.attendance.dto.AttendanceRequest;
+import se.mistral.backend.child.ChildRepository;
+import se.mistral.backend.attendance.dto.AttendanceDto;
+
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -20,29 +18,8 @@ public class AttendanceService {
     private final AttendanceRepository attendanceRepository;
     private final ChildRepository childRepository;
 
-    public AttendanceDtoList getAttendances(AttendancesRequest request) {
-        List<AttendanceListItem> list = attendanceRepository.findByDate(request.date())
-            .stream()
-            .map(attendance -> new AttendanceListItem(
-                        attendance.getChild().getId(),
-                        attendance.getChild().getName(),
-                        attendance.getPresent()
-                        )
-                )
-            .toList();
-        return new AttendanceDtoList(list, list.size());
-    }
-
-    public AttendanceDto getAttendance(AttendanceFetchRequest request) {
-        Attendance attendance = attendanceRepository.findByChildIdAndDate(request.childId(), request.date())
-            .orElseGet(() -> attendanceRepository.save(Attendance.builder()
-                        .date(request.date())
-                        .child(childRepository.findById(request.childId()).orElseThrow(() -> new NotFoundException("Child not found")))
-                        .present(false)
-                        .build()
-                        )
-                    );
-        return new AttendanceDto(attendance.getPresent());
+    public AttendanceDto getAttendance(Long childId, LocalDate date) {
+        return attendanceRepository.findPresenceByChildIdAndDate(childId, date);
     }
 
     public AttendanceDto updateAttendance(AttendanceRequest request) {
@@ -52,9 +29,9 @@ public class AttendanceService {
                     .child(childRepository.findById(request.childId()).orElseThrow(() -> new NotFoundException("Child not found")))
                     .present(request.present())
                     .build()
-                   );
+                );
         attendance.setPresent(request.present());
-        attendanceRepository.save(attendance);      // throws ObjectOptimisticLockingFailureException on conflict
+        attendance = attendanceRepository.save(attendance);      // throws ObjectOptimisticLockingFailureException on conflict
         return new AttendanceDto(attendance.getPresent());
     }
 }
