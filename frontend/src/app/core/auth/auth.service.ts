3718bda53540f2c9bf41a8f3214a7657bb16c758
FrickTown <jakobaefrick@gmail.com>
2026-04-17 12:14:19 +0200

    fixed eslint errors

--- frontend/src/app/core/auth/auth.service.ts
+++ frontend/src/app/core/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { firstValueFrom, Observable, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
 
 interface LoginRequest {
 	email: string;
