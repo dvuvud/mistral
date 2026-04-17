6e8d462e25585fffd04d8c6abf61d88605385df7
FrickTown <jakobaefrick@gmail.com>
2026-04-17 17:18:49 +0200

    Added admin page route and boilerplate

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginPage } from './routes/login-page/login-page';
 import { MainPage } from './routes/main-page/main-page';
 import { authGuard } from './auth-guard';
+import { AdminPage } from './routes/admin-page/admin-page';
 
 export const routes: Routes = [
   {
@@ -13,5 +14,9 @@ export const routes: Routes = [
     canActivate: [authGuard],
     path: 'app', // Detta innebär att localhost:4200/app leder till main-page komponenten.
     component: MainPage
+  },
+  {
+    path: 'admin',
+    component: AdminPage
   }
 ];
