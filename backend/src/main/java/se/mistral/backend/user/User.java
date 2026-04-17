dad5f64631efc43ac852477ea83ecb8c8a8084f9
dvuvud <carldavid.buhrman@gmail.com>
2026-04-17 04:58:37 +0200

    use sun checks for stricter validation

--- backend/src/main/java/se/mistral/backend/user/User.java
+++ backend/src/main/java/se/mistral/backend/user/User.java
@@ -20,7 +20,7 @@ import java.util.List;
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
-                                                                                                                    public class User implements UserDetails {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
