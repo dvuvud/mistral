b97052107612ec00d71178bb148d971c8c109d98
Alec <alec@andin.se>
2026-04-21 00:18:00 +0200

    ADD AuthTests file

---
+++ backend/src/test/java/se/mistral/backend/AuthTests.java
@@ -0,0 +1,21 @@
+package se.mistral.backend;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import se.mistral.backend.auth.AuthService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+public class AuthTests {
+    @Autowired
+    private AuthService authService;
+
+    @Test
+    void test() {
+        assertThat(1 + 1).isEqualTo(2);
+    }
+}
