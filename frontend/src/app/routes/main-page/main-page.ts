7b4f26e15e82db3d565a90f3c601253aa9ab08fc
FrickTown <jakobaefrick@gmail.com>
2026-04-20 20:21:38 +0200

    Added websocket message handler, with small testbutton (set child 1 to true)

--- frontend/src/app/routes/main-page/main-page.ts
+++ frontend/src/app/routes/main-page/main-page.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal, ViewEncapsulation } from '@angular/core';
+import { Component, inject, signal, ViewEncapsulation, viewChild } from '@angular/core';
 import { MainPanel } from './main-panel/main-panel';
 import { Child } from '../../core/child/child.service';
 import { Router } from '@angular/router';
@@ -6,6 +6,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTab, MatTabGroup } from '@angular/material/tabs';
+import { AttendanceService } from '../../core/child/attendance.service';
 
 @Component({
   selector: 'main-page',
@@ -20,12 +21,18 @@ import { MatTab, MatTabGroup } from '@angular/material/tabs';
   encapsulation: ViewEncapsulation.Emulated,
 
   templateUrl: './main-page.html',
-  styleUrl: './main-page.scss',
+  styleUrl: './main-page.scss'
 })
 export class MainPage {
   childSignal = signal<Child>({ name: '', id: 0, date: "", present: false });
-
   private router = inject(Router);
+  mainPanel = viewChild.required(MainPanel);
+
+  testMessage = '{"childID": 1, "present": "true"}';
+
+  handleWebsocketMessage(message: string) {
+    this.mainPanel().handleWebsocketMessage(message);
+  }
 
   logout() {
     document.cookie = 'jwtToken=""';
