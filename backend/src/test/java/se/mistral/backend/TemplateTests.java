a2ee7231b47f0f519c01396db7e0e4e89230f0af
Alec <alec@andin.se>
2026-04-21 00:32:58 +0200

    Fix Make lint happy

--- backend/src/test/java/se/mistral/backend/TemplateTests.java
+++ backend/src/test/java/se/mistral/backend/TemplateTests.java
@@ -2,16 +2,15 @@ package se.mistral.backend;
 
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
+// import org.springframework.beans.factory.annotation.Autowired;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
 public class TemplateTests {
     // if testing a service generate one using @Autowired as shown below
-    // You can comment them out and replace _ with the relevant Sevice
+    // You can comment them out and replace _ with the relevant Service
     //@Autowired
     //private _Service _Service;
     @Test
