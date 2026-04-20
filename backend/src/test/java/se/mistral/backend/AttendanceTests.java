8e20d842f9338cbefaa5c5f70e195c70ab235df6
Alec <alec@andin.se>
2026-04-21 00:18:17 +0200

    ADD AttendanceTests file

---
+++ backend/src/test/java/se/mistral/backend/AttendanceTests.java
@@ -0,0 +1,21 @@
+package se.mistral.backend;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import se.mistral.backend.attendance.AttendanceService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+public class AttendanceTests {
+    @Autowired
+    private AttendanceService attendanceService;
+
+    @Test
+    void test() {
+        assertThat(1 + 1).isEqualTo(2);
+    }
+}
