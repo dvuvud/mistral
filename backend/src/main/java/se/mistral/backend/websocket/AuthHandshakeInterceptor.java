43858207dd0a9764201b8864d9dcc1f7e9dd0582
dvuvud <carldavid.buhrman@gmail.com>
2026-04-20 23:27:15 +0200

    implement an authentication handshake interceptor to allow access to the token for the handler via attributes

---
+++ backend/src/main/java/se/mistral/backend/websocket/AuthHandshakeInterceptor.java
@@ -0,0 +1,30 @@
+package se.mistral.backend.websocket;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+@Component
+public class AuthHandshakeInterceptor implements HandshakeInterceptor {
+
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response,
+                                   WebSocketHandler wsHandler, Map<String, Object> attributes) {
+
+        List<String> auth = request.getHeaders().get("Authorization");
+        if (auth != null && !auth.isEmpty()) {
+            attributes.put("token", auth.get(0).substring(7)); // strip "Bearer " and put in attributes for later use
+        }
+        return true; // always let it through, we'll close in the handler if invalid
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response,
+                               WebSocketHandler wsHandler, Exception exception) {} // do nothing after (for now at least)
+}
+
