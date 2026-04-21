e66feb37a69a7081d53938d119438f1cae2c5bb6
Hiba Ahmad  hiah5302 <hiba.ahmad.5302@student.uu.se>
2026-04-21 16:26:23 +0200

    Implementationen av websocket

--- frontend/src/app/routes/main-page/main-page.ts
+++ frontend/src/app/routes/main-page/main-page.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal, ViewEncapsulation, viewChild } from '@angular/core';
+import { Component, inject, signal, ViewEncapsulation, viewChild, OnDestroy, OnInit } from '@angular/core';
 import { MainPanel } from './main-panel/main-panel';
 import { Child } from '../../core/child/child.service';
 import { Router } from '@angular/router';
@@ -7,6 +7,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTab, MatTabGroup } from '@angular/material/tabs';
 import { AttendanceService } from '../../core/child/attendance.service';
+import { WebsocketService } from '../../core/websocket/websocket.service';
 
 @Component({
   selector: 'main-page',
@@ -23,12 +24,24 @@ import { AttendanceService } from '../../core/child/attendance.service';
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss'
 })
-export class MainPage {
+export class MainPage implements OnInit, OnDestroy {
+  
   childSignal = signal<Child>({ name: '', id: 0, date: "", present: false });
   private router = inject(Router);
+  private socketService = inject(WebsocketService);
   mainPanel = viewChild.required(MainPanel);
 
   testMessage = '{"childID": 1, "present": "true"}';
+
+  ngOnInit() {
+    this.socketService.getMessages().subscribe((message) => {
+      this.handleWebsocketMessage(message);
+    });
+  }
+
+  ngOnDestroy() {
+    this.socketService.disconnect();
+  }
 
   handleWebsocketMessage(message: string) {
     this.mainPanel().handleWebsocketMessage(message);
