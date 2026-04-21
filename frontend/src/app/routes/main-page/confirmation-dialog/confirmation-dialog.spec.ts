b9bd9fd71d9ced47b748e69bc1fded6c536e55b9
FrickTown <jakobaefrick@gmail.com>
2026-04-21 22:08:02 +0200

    Fixed git rebase merge artifacts. Probably missed a couple somewhere

--- frontend/src/app/routes/main-page/confirmation-dialog/confirmation-dialog.spec.ts
+++ frontend/src/app/routes/main-page/confirmation-dialog/confirmation-dialog.spec.ts
@@ -9,12 +9,7 @@ describe('ConfirmationDialog', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ConfirmationDialog]
-    })
-<<<<<<< HEAD
-      .compileComponents();
-=======
-    .compileComponents();
->>>>>>> d823078 (La till basic bekräftelseruta, den är inte stylad än. Stor commit förlåt David <3)
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ConfirmationDialog);
     component = fixture.componentInstance;
--- frontend/src/app/routes/main-page/confirmation-dialog/confirmation-dialog.ts
+++ frontend/src/app/routes/main-page/confirmation-dialog/confirmation-dialog.ts
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { Component, inject } from '@angular/core';
-=======
-import { Component } from '@angular/core';
->>>>>>> d823078 (La till basic bekräftelseruta, den är inte stylad än. Stor commit förlåt David <3)
-=======
-import { Component, inject } from '@angular/core';
->>>>>>> e15eee9 (Fixade linting errors)
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 
@@ -18,18 +10,8 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './confirmation-dialog.scss',
 })
 export class ConfirmationDialog {
-  
-<<<<<<< HEAD
-<<<<<<< HEAD
-  private dialogRef = inject(MatDialogRef<ConfirmationDialog>);
 
-=======
-  constructor(private dialogRef: MatDialogRef<ConfirmationDialog>) {}
->>>>>>> d823078 (La till basic bekräftelseruta, den är inte stylad än. Stor commit förlåt David <3)
-=======
   private dialogRef = inject(MatDialogRef<ConfirmationDialog>);
-
->>>>>>> e15eee9 (Fixade linting errors)
 
   onConfirm() {
     this.dialogRef.close(true);
