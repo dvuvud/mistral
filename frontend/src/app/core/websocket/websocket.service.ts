a243e04d8c50f3b3802896ce8345c29a9af59b67
FrickTown <jakobaefrick@gmail.com>
2026-04-21 20:03:57 +0200

    WebSocket bindings for attendance completed, generalized WebSocket service for future bindings

--- frontend/src/app/core/websocket/websocket.service.ts
+++ frontend/src/app/core/websocket/websocket.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
-import { Socket } from 'ngx-socket-io';
 import { Observable, Subject } from 'rxjs';
 
 type WsType = 'subscribe' | 'unsubscribe' | 'message';
 
-interface webPayload {
+export type WsMessageType = WsAttendanceMessage | WsJournalMessage;
+
+interface WsPayload {
   type: WsType;
   room: string;
-  body?: unknown;
+  body: WsMessageType;
+}
+
+export interface WsAttendanceMessage {
+  childId: number,
+  present: boolean
+}
+
+export interface WsJournalMessage {
+  journalId: number,
+  present: boolean
 }
 
 @Injectable({
@@ -16,8 +27,8 @@ interface webPayload {
 
 export class WebsocketService {
   private socket: WebSocket | null = null;
-  private messages: Subject<any> = new Subject();
-  private roomName: string = '';
+  private messages = new Subject<WsMessageType>();
+  private roomName = '';
 
   connect(url: string, roomName: string): void {
     const token = localStorage.getItem("token");
@@ -30,8 +41,25 @@ export class WebsocketService {
     };
 
     this.socket.onmessage = (event) => {
-      console.log("Received:", event.data);
-      this.messages.next(JSON.parse(event.data));
+      // Parse outer JSON object
+      const rawJSON = JSON.parse(event.data);
+      if (rawJSON.body == undefined) {
+        console.error("WebSocket message contains no body!");
+        return;
+      }
+
+      // Parse inner JSON object (nested body property)
+      rawJSON.body = JSON.parse(rawJSON.body);
+      const payload: WsPayload = rawJSON;
+      if (payload.room != this.roomName) {
+        console.error(`WebSocket message room mismatch! ([Service] = ${this.roomName} vs [Message] = ${payload.room})`);
+        return;
+      }
+
+      // Broadcast the parsed JSON object
+      console.log(`Received from WebSocket (room = ${this.roomName}):`);
+      console.log(payload.body);
+      this.messages.next(payload.body);
     };
 
     this.socket.onclose = (event) => {
@@ -47,11 +75,13 @@ export class WebsocketService {
     }
   }
 
-  sendMessage(message: string): void {
-    this.socket?.send(JSON.stringify({ type: 'message', room: this.roomName, body: message }));
+  sendMessage(message: WsMessageType): void {
+    const msgAsString = JSON.stringify({ type: 'message', room: this.roomName, body: JSON.stringify(message) });
+    console.log("Sending off ", msgAsString)
+    this.socket?.send(msgAsString);
   }
 
-  getMessages(): Observable<any> {
+  getMessages(): Observable<WsMessageType> {
     return this.messages.asObservable();
   }
 }
--- frontend/src/app/routes/main-page/child-display/child-display.ts
+++ frontend/src/app/routes/main-page/child-display/child-display.ts
@@ -1,12 +1,19 @@
-import { Component, model } from '@angular/core';
+import { Component, EventEmitter, model, Output } from '@angular/core';
 import { Child } from '../../../core/child/child.service';
+import { AttendanceBox } from "../attendance-box/attendance-box";
+import { WsMessageType } from '../../../core/websocket/websocket.service';
 
 @Component({
   selector: 'child-display',
-  imports: [],
+  imports: [AttendanceBox],
   templateUrl: './child-display.html',
   styleUrl: './child-display.scss',
 })
 export class ChildDisplay {
   childSignal = model.required<Child>();
+
+  @Output() attendanceChangeEvent = new EventEmitter();
+  wsUpdateAttendance(msg: WsMessageType) {
+    this.attendanceChangeEvent.emit(msg);
+  }
 }
