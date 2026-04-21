3737a41844e3c3cd7aa464fdfedd68d8f8c0c0f0
Axel Segerholm <Axel.segerholm@gmail.com>
2026-04-21 08:51:05 +0200

    Fixade linting errors

--- frontend/src/app/routes/main-page/confirmation-dialog/confirmation-dialog.ts
+++ frontend/src/app/routes/main-page/confirmation-dialog/confirmation-dialog.ts
@@ -1,8 +1,12 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { Component, inject } from '@angular/core';
 =======
 import { Component } from '@angular/core';
 >>>>>>> d823078 (La till basic bekräftelseruta, den är inte stylad än. Stor commit förlåt David <3)
+=======
+import { Component, inject } from '@angular/core';
+>>>>>>> e15eee9 (Fixade linting errors)
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 
@@ -16,11 +20,16 @@ import { MatButtonModule } from '@angular/material/button';
 export class ConfirmationDialog {
   
 <<<<<<< HEAD
+<<<<<<< HEAD
   private dialogRef = inject(MatDialogRef<ConfirmationDialog>);
 
 =======
   constructor(private dialogRef: MatDialogRef<ConfirmationDialog>) {}
 >>>>>>> d823078 (La till basic bekräftelseruta, den är inte stylad än. Stor commit förlåt David <3)
+=======
+  private dialogRef = inject(MatDialogRef<ConfirmationDialog>);
+
+>>>>>>> e15eee9 (Fixade linting errors)
 
   onConfirm() {
     this.dialogRef.close(true);
