52874fba6d211e78ac563270252c3348fddc3ab3
Imran Abdihakim <imuu.dv@outlook.com>
2026-04-21 14:53:14 +0200

    add admin page layout with components

--- frontend/src/app/routes/admin-page/admin-page.ts
+++ frontend/src/app/routes/admin-page/admin-page.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
-import { MatCard, MatCardHeader, MatCardContent } from "@angular/material/card";
+import { MatCard, MatCardHeader, MatCardContent, MatCardModule } from "@angular/material/card";
+import { AddChildForm } from './add-child-form/add-child-form';
+import { ChildAdminList } from './child-admin-list/child-admin-list';
+import { CommonModule } from '@angular/common';
+import { Group } from './group/group';
 
 @Component({
   selector: 'admin-page',
-  imports: [MatCard, MatCardHeader, MatCardContent],
+  imports: [
+    MatCard, 
+    MatCardHeader, 
+    MatCardContent,
+    MatCardModule, 
+    AddChildForm, 
+    ChildAdminList,
+    Group,
+    CommonModule
+  ],
   templateUrl: './admin-page.html',
   styleUrl: './admin-page.scss',
 })
