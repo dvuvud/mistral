54851731ca3e5486e6e31f47e9869e335d2f7e5e
MuschBenne <sjostrand.benjamin@gmail.com>
2026-04-21 13:25:38 +0200

    Update table for children

--- backend/src/main/java/se/mistral/backend/child/Child.java
+++ backend/src/main/java/se/mistral/backend/child/Child.java
@@ -3,22 +3,26 @@ package se.mistral.backend.child;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.CascadeType;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.persistence.JoinColumn;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import java.util.List;
 import se.mistral.backend.attendance.Attendance;
+import se.mistral.backend.grupp.Grupp;
+import jakarta.persistence.Index;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "children")
+@Table(name = "children", indexes = @Index(name = "idx_child_name", columnList = "grupp_id"))
 public class Child {
 
     @Id
@@ -27,6 +31,10 @@ public class Child {
 
     @OneToMany(mappedBy = "child", cascade = CascadeType.REMOVE)
     private List<Attendance> attendances;
+
+    @ManyToOne
+    @JoinColumn(name = "grupp_id")
+    private Grupp grupp;
 
     @NotBlank
     private String name;
