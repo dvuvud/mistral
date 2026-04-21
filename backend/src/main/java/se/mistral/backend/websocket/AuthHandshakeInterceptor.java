52c43feafee62b8e332b7adfd9794120610d93b6
dvuvud <carldavid.buhrman@gmail.com>
2026-04-21 12:24:35 +0200

    switch to using query params over header for simplicity

--- backend/src/main/java/se/mistral/backend/websocket/AuthHandshakeInterceptor.java
+++ backend/src/main/java/se/mistral/backend/websocket/AuthHandshakeInterceptor.java
@@ -1,6 +1,5 @@
 package se.mistral.backend.websocket;
 
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.server.ServerHttpRequest;
@@ -8,18 +7,25 @@ import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 public class AuthHandshakeInterceptor implements HandshakeInterceptor {
+
 
     @Override
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response,
                                    WebSocketHandler wsHandler, Map<String, Object> attributes) {
 
-        List<String> auth = request.getHeaders().get("Authorization");
-        if (auth != null && !auth.isEmpty()) {
-            attributes.put("token", auth.get(0).substring("Bearer ".length())); // strip "Bearer " and put in attributes for later use
+        var params = UriComponentsBuilder.fromUri(request.getURI())
+                                     .build()
+                                     .getQueryParams();
+
+        String token = params.getFirst("token");
+        if (token != null) {
+            attributes.put("token", token);
         }
+
         return true; // always let it through, we'll close in the handler if invalid
     }
 
