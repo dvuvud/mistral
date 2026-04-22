59bce4ff847e1d847ae89d6e3e5c99c3b91f22e0
Ivan <ivan.brolin.5150@student.uu.se>
2026-04-22 13:00:55 +0200

    add basic websocket for the journal

--- frontend/src/app/routes/main-page/main-live-journal/main-live-journal.ts
+++ frontend/src/app/routes/main-page/main-live-journal/main-live-journal.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatFormField } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatTabGroup, MatTab } from "@angular/material/tabs";
+import { WebsocketService, WsMessageType } from '../../../core/websocket/websocket.service';
 
 @Component({
   selector: 'main-live-journal',
@@ -10,5 +11,16 @@ import { MatTabGroup, MatTab } from "@angular/material/tabs";
   styleUrl: './main-live-journal.scss',
 })
 export class MainLiveJournal {
+  private journalSocket = new WebsocketService;
+  
+  ngOnInit() {
+    this.journalSocket.connect("ws://localhost:8080/ws", "journal");
+    this.journalSocket.getMessages().subscribe((message) => {
+      // TODO
+    });
+  }
 
+  ngOnDestroy() {
+    this.journalSocket.disconnect();
+  }
 }
