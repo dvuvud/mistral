f5dcd25e02691241afaa0a62f926f874b2fddcb8
Ivan <ivan.brolin.5150@student.uu.se>
2026-04-22 13:00:55 +0200

    add basic websocket for the journal

--- frontend/src/app/routes/main-page/main-live-journal/main-live-journal.ts
+++ frontend/src/app/routes/main-page/main-live-journal/main-live-journal.ts
@@ -1,13 +1,8 @@
-import { Component, OnDestroy, OnInit, signal, Input, OnChanges, SimpleChanges, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatFormField } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatTabGroup, MatTab } from "@angular/material/tabs";
-import { FormsModule } from '@angular/forms';
-import { operation, WebsocketService, WsJournalMessage, WsJournalResponse } from '../../../core/websocket/websocket.service';
-import { Child } from '../../../core/child/child.service';
-import { textDiff, Diff } from './textDiff';
-import { operationalTransformation } from './operationalTransformation';
-import { JournalService } from '../../../core/journal/journal.service';
+import { WebsocketService, WsMessageType } from '../../../core/websocket/websocket.service';
 
 @Component({
   selector: 'main-live-journal',
@@ -17,116 +12,15 @@ import { JournalService } from '../../../core/journal/journal.service';
 })
 export class MainLiveJournal {
   private journalSocket = new WebsocketService;
-  private differ = new textDiff();
-  private operationalTransformer = new operationalTransformation();
-  private journalService = inject(JournalService);
-
-  text = signal('');
-  prevText = '';
-
-  serverRevision = 0;
-  sequence = 0;
-
-  inFlight: operation[] = [];
-
+  
   ngOnInit() {
-    let roomDest = 'journal:' + this.child.id + ':' + new Date().toISOString().split('T')[0];
-    this.journalSocket.connect("ws://localhost:8080/ws", roomDest);
-
-    this.journalSocket.getMessages().subscribe(data => {
-      console.log(this.inFlight);
-      console.log("recieved: ", data)
-      const response = data as WsJournalResponse;
-      let incoming = response.operation;
-      if (this.isMyOwnAck(response)) {
-        this.inFlight.shift();
-      } else {
-        for (const myOp of this.inFlight) {
-          incoming = this.operationalTransformer.transformClient(incoming, myOp);
-        }
-        this.applyToLocalContent(incoming);
-      }
-      this.serverRevision = response.serverRevision;
+    this.journalSocket.connect("ws://localhost:8080/ws", "journal");
+    this.journalSocket.getMessages().subscribe((message) => {
+      // TODO
     });
-  }
-
-  isMyOwnAck(msg: WsJournalResponse): boolean {
-    // TODO: myId ännu inte implementerat
-    return true;
-    //return msg.userId === myID && this.inFlight.length > 0;
-  }
-
-  applyToLocalContent(incoming: operation): void {
-    let text: string = this.text();
-    const pos: number = incoming.position;
-    switch(incoming.type) {
-      case 'INSERT':
-        this.text.set(text.slice(0, pos) + incoming.text + text.slice(pos, text.length));
-        break;
-      case 'DELETE':
-        this.text.set(text.slice(0, pos) + text.slice(pos + 1, text.length));
-        break;
-    }
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {  
-    if (changes['child'] && !changes['child'].firstChange) {
-      this.journalService.getJournal(this.child.id).subscribe({
-        next: (data) => {
-          console.log(data);
-          this.text.set(data.content);
-          this.serverRevision = data.serverRevision;
-          this.sequence = 0;
-          this.inFlight = [];
-        }
-      });
-
-      let roomDest = 'journal:' + this.child.id + ':' + new Date().toISOString().split('T')[0];
-      console.log(roomDest);
-      this.journalSocket.changeRoom(roomDest);
-      this.prevText = '';
-    }
   }
 
   ngOnDestroy() {
     this.journalSocket.disconnect();
-  }
-
-  onInput(event: Event) {
-    this.sequence++;
-
-    const newValue = (event.target as HTMLTextAreaElement).value;
-    this.prevText = this.text();
-    let diff: Diff = this.differ.getDiff(this.prevText, newValue);
-    this.text.set(newValue);
-    let operation;
-    switch (diff.operation) {
-      case 'DELETE':
-        operation = {
-          type: 'DELETE',
-          position: diff.idx,
-          length: 1
-        }
-        break;
-      case 'INSERT':
-        operation = {
-          type: 'INSERT',
-          position: diff.idx,
-          text: diff.value
-        }
-        break;
-    }
-
-    const message = {
-      type: "DOC_OPERATION",
-      room: 'journal:' + this.child.id + ':' + new Date().toISOString().split('T')[0],
-      clientRevision: this.serverRevision,
-      operation: operation,
-      sequence: this.sequence
-    }
-
-    this.inFlight.push(operation as operation);
-
-    this.journalSocket.sendMessageJournal(message as WsJournalMessage);
   }
 }
