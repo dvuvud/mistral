4fb0802fcdd4b9c39c2e2162c849119f187b3eac
Ivan <ivan.brolin.5150@student.uu.se>
2026-04-22 14:18:54 +0200

    add textarea binding & differ

--- frontend/src/app/routes/main-page/main-live-journal/main-live-journal.ts
+++ frontend/src/app/routes/main-page/main-live-journal/main-live-journal.ts
@@ -1,18 +1,24 @@
-import { Component, inject } from '@angular/core';
+import { Component, effect, OnDestroy, OnInit, signal } from '@angular/core';
 import { MatFormField } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatTabGroup, MatTab } from "@angular/material/tabs";
-import { WebsocketService, WsMessageType } from '../../../core/websocket/websocket.service';
+import { FormsModule } from '@angular/forms';
+import { WebsocketService } from '../../../core/websocket/websocket.service';
+import { textDiff } from './textDiff';
 
 @Component({
   selector: 'main-live-journal',
-  imports: [MatFormField, MatInput, MatTabGroup, MatTab],
+  imports: [MatFormField, MatInput, MatTabGroup, MatTab, FormsModule],
   templateUrl: './main-live-journal.html',
   styleUrl: './main-live-journal.scss',
 })
 export class MainLiveJournal {
   private journalSocket = new WebsocketService;
-  
+  private differ = new textDiff();
+
+  text = signal('');
+  prevText = '';
+
   ngOnInit() {
     this.journalSocket.connect("ws://localhost:8080/ws", "journal");
     this.journalSocket.getMessages().subscribe((message) => {
@@ -22,5 +28,14 @@ export class MainLiveJournal {
 
   ngOnDestroy() {
     this.journalSocket.disconnect();
+  }
+
+  onInput(event: Event) {
+    const newValue = (event.target as HTMLTextAreaElement).value;
+    this.prevText = this.text(); // capture before update
+    console.log("previous text: " + this.prevText);
+    console.log("new text: " + newValue);
+    console.log(this.differ.getDiff(this.prevText, newValue));
+    this.text.set(newValue);
   }
 }
