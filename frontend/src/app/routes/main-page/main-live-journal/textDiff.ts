a768673155d004b2d902b320bfd0d978d895656b
Ivan <ivan.brolin.5150@student.uu.se>
2026-04-22 16:52:24 +0200

    add differ

---
+++ frontend/src/app/routes/main-page/main-live-journal/textDiff.ts
@@ -0,0 +1,32 @@
+export type Diff = {
+    operation: 'Ins' | 'Del';
+    idx: number;
+    value: string;
+}
+
+export class textDiff {
+    getDiff(prevText: string, newText: string): Diff {
+        const operation = prevText.length < newText.length ? 'Ins' : 'Del';
+        let diff: Diff = {operation: operation, idx: 0, value: ''}
+        switch (operation) {
+            case 'Del':
+                diff.idx = this.getDiffIdx(prevText, newText);
+                diff.value = prevText.charAt(diff.idx);
+                break;
+            case 'Ins':
+                diff.idx = this.getDiffIdx(prevText, newText);
+                diff.value = newText.charAt(diff.idx);
+                break;
+        }
+        return diff;
+    }
+
+    getDiffIdx(prevText: string, newText: string): number {
+        let idx: number = 0;
+        while (prevText.charAt(idx) == newText.charAt(idx)) {
+            idx++;
+            if (idx > Math.max(prevText.length, newText.length)) break;
+        }
+        return idx;
+    }
+}
