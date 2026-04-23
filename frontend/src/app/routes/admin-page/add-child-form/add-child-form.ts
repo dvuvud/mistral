42321da75f5f39419949b205db98c3b411e858a3
Imran Abdihakim <imuu.dv@outlook.com>
2026-04-23 03:20:25 +0200

    fix child-list not loading to admin page

--- frontend/src/app/routes/admin-page/add-child-form/add-child-form.ts
+++ frontend/src/app/routes/admin-page/add-child-form/add-child-form.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ChildService } from '../../../core/child/child.service';
 
 @Component({
   selector: 'add-child-form',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+
+  ],
   templateUrl: './add-child-form.html',
   styleUrl: './add-child-form.scss',
 })
 export class AddChildForm {
+  private fb = inject(FormBuilder);
+
+  private childService = inject(ChildService);
+
+
+  //output?
+
+  form = this.fb.group({
+    name: ["", Validators.required]
+    //id?
+  })
+
+
+  onSubmit(): void {
+
+    
+  }
 
 }
