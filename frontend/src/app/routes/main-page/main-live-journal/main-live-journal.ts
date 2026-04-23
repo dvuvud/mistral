bae3f7ec0e385eec73cdb5c4a17881bc45652789
Ivan <ivan.brolin.5150@student.uu.se>
2026-04-23 10:46:29 +0200

    add operational transformation in frontend

--- frontend/src/app/routes/main-page/main-live-journal/main-live-journal.ts
+++ frontend/src/app/routes/main-page/main-live-journal/main-live-journal.ts
@@ -3,9 +3,10 @@ import { MatFormField } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatTabGroup, MatTab } from "@angular/material/tabs";
 import { FormsModule } from '@angular/forms';
-import { WebsocketService, WsJournalMessage } from '../../../core/websocket/websocket.service';
+import { operation, WebsocketService, WsJournalMessage, WsJournalResponse } from '../../../core/websocket/websocket.service';
 import { Child } from '../../../core/child/child.service';
 import { textDiff, Diff } from './textDiff';
+import { operationalTransformation } from './operationalTransformation';
 import { JournalService } from '../../../core/journal/journal.service';
 
 @Component({
@@ -17,22 +18,46 @@ import { JournalService } from '../../../core/journal/journal.service';
 export class MainLiveJournal {
   private journalSocket = new WebsocketService;
   private differ = new textDiff();
+  private operationalTransformer = new operationalTransformation();
   private journalService = inject(JournalService);
 
   text = signal('');
   prevText = '';
 
   serverRevision = 0;
-
   sequence = 0;
+
+  inFlight: operation[] = [];
 
   ngOnInit() {
     let roomDest = 'journal:' + this.child.id + ':' + new Date().toISOString().split('T')[0];
     this.journalSocket.connect("ws://localhost:8080/ws", roomDest);
 
     this.journalSocket.getMessages().subscribe(data => {
-      console.log(data);
-    })
+      console.log(this.inFlight);
+      console.log("recieved: ", data)
+      const response = data as WsJournalResponse;
+      let incoming = response.operation;
+      if (this.isMyOwnAck(response)) {
+        this.inFlight.shift();
+      } else {
+        for (const myOp of this.inFlight) {
+          incoming = this.operationalTransformer.transformClient(incoming, myOp);
+        }
+        this.applyToLocalContent(incoming);
+      }
+      this.serverRevision = response.serverRevision;
+    });
+  }
+
+  isMyOwnAck(msg: WsJournalResponse): boolean {
+    // TODO: myId ännu inte implementerat
+    return true;
+    //return msg.userId === myID && this.inFlight.length > 0;
+  }
+
+  applyToLocalContent(incoming: operation): void {
+
   }
 
   ngOnChanges(changes: SimpleChanges): void {  
@@ -43,6 +68,7 @@ export class MainLiveJournal {
           this.text.set(data.content);
           this.serverRevision = data.serverRevision;
           this.sequence = 0;
+          this.inFlight = [];
         }
       });
 
@@ -88,8 +114,9 @@ export class MainLiveJournal {
       clientRevision: this.serverRevision,
       operation: operation,
       sequence: this.sequence
-      
     }
+
+    this.inFlight.push(operation as operation);
 
     this.journalSocket.sendMessageJournal(message as WsJournalMessage);
   }
