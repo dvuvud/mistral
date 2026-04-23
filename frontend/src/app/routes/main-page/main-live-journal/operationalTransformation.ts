2b75f0caf72846dc265f28117a35d0a17d169769
Ivan <ivan.brolin.5150@student.uu.se>
2026-04-23 10:46:29 +0200

    add operational transformation in frontend

---
+++ frontend/src/app/routes/main-page/main-live-journal/operationalTransformation.ts
@@ -0,0 +1,46 @@
+import { operation } from "../../../core/websocket/websocket.service";
+
+export class operationalTransformation {
+    transformClient(inOp: operation, flightOp: operation): operation {
+        switch (inOp.type) {   
+            case 'INSERT':
+                return this.transformInsert(inOp, flightOp);
+            case 'DELETE':
+                return this.transformDelete(inOp, flightOp);
+        }
+    }
+
+    transformInsert(inOp: operation, flightOp: operation): operation {
+        let resOp = inOp;
+        switch (flightOp.type) {   
+            case 'INSERT':
+                if (flightOp.position < inOp.position) {
+                    resOp.position++;
+                }
+                break;
+            case 'DELETE':
+                if (flightOp.position < inOp.position) {
+                    resOp.position--;
+                }
+                break;
+        }
+        return resOp;
+    }
+
+    transformDelete(inOp: operation, flightOp: operation): operation {
+        let resOp = inOp;
+        switch (flightOp.type) {   
+            case 'INSERT':
+                if (flightOp.position < inOp.position) {
+                    resOp.position++;
+                }
+                break;
+            case 'DELETE':
+                if (flightOp.position < inOp.position) {
+                    resOp.position--;
+                }
+                break;
+        }
+        return resOp;
+    }
+}
