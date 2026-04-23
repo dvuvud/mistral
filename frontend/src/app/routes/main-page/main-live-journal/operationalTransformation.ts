4f01477cbf0e8a43ac6be42be8e49e52ab2307c2
Ivan <ivan.brolin.5150@student.uu.se>
2026-04-23 16:26:25 +0200

    fix linting passed & roomName is private

--- frontend/src/app/routes/main-page/main-live-journal/operationalTransformation.ts
+++ frontend/src/app/routes/main-page/main-live-journal/operationalTransformation.ts
@@ -1,46 +1,46 @@
 import { operation } from "../../../core/websocket/websocket.service";
 
 export class operationalTransformation {
-    transformClient(inOp: operation, flightOp: operation): operation {
-        switch (inOp.type) {   
-            case 'INSERT':
-                return this.transformInsert(inOp, flightOp);
-            case 'DELETE':
-                return this.transformDelete(inOp, flightOp);
-        }
+  transformClient(inOp: operation, flightOp: operation): operation {
+    switch (inOp.type) {   
+      case 'INSERT':
+        return this.transformInsert(inOp, flightOp);
+      case 'DELETE':
+        return this.transformDelete(inOp, flightOp);
     }
+  }
 
-    transformInsert(inOp: operation, flightOp: operation): operation {
-        let resOp = inOp;
-        switch (flightOp.type) {   
-            case 'INSERT':
-                if (flightOp.position < inOp.position) {
-                    resOp.position++;
-                }
-                break;
-            case 'DELETE':
-                if (flightOp.position < inOp.position) {
-                    resOp.position--;
-                }
-                break;
+  transformInsert(inOp: operation, flightOp: operation): operation {
+    const resOp = inOp;
+    switch (flightOp.type) {   
+      case 'INSERT':
+        if (flightOp.position < inOp.position) {
+          resOp.position++;
         }
-        return resOp;
+        break;
+      case 'DELETE':
+        if (flightOp.position < inOp.position) {
+          resOp.position--;
+        }
+        break;
     }
+    return resOp;
+  }
 
-    transformDelete(inOp: operation, flightOp: operation): operation {
-        let resOp = inOp;
-        switch (flightOp.type) {   
-            case 'INSERT':
-                if (flightOp.position < inOp.position) {
-                    resOp.position++;
-                }
-                break;
-            case 'DELETE':
-                if (flightOp.position < inOp.position) {
-                    resOp.position--;
-                }
-                break;
+  transformDelete(inOp: operation, flightOp: operation): operation {
+    const resOp = inOp;
+    switch (flightOp.type) {   
+      case 'INSERT':
+        if (flightOp.position < inOp.position) {
+          resOp.position++;
         }
-        return resOp;
+        break;
+      case 'DELETE':
+        if (flightOp.position < inOp.position) {
+          resOp.position--;
+        }
+        break;
     }
+    return resOp;
+  }
 }
--- frontend/src/app/routes/main-page/main-live-journal/textDiff.ts
+++ frontend/src/app/routes/main-page/main-live-journal/textDiff.ts
@@ -1,34 +1,32 @@
-import { operation } from "../../../core/websocket/websocket.service";
-
-export type Diff = {
+export interface Diff {
     operation: 'INSERT' | 'DELETE';
     idx: number;
     value: string;
 }
 
 export class textDiff {
-    getDiff(prevText: string, newText: string): Diff {
-        const operation = prevText.length < newText.length ? 'INSERT' : 'DELETE';
-        let diff: Diff = {operation: operation, idx: 0, value: ''}
-        switch (operation) {
-            case 'DELETE':
-                diff.idx = this.getDiffIdx(prevText, newText);
-                diff.value = prevText.charAt(diff.idx);
-                break;
-            case 'INSERT':
-                diff.idx = this.getDiffIdx(prevText, newText);
-                diff.value = newText.charAt(diff.idx);
-                break;
-        }
-        return diff;
+  getDiff(prevText: string, newText: string): Diff {
+    const operation = prevText.length < newText.length ? 'INSERT' : 'DELETE';
+    const diff: Diff = {operation: operation, idx: 0, value: ''}
+    switch (operation) {
+      case 'DELETE':
+        diff.idx = this.getDiffIdx(prevText, newText);
+        diff.value = prevText.charAt(diff.idx);
+        break;
+      case 'INSERT':
+        diff.idx = this.getDiffIdx(prevText, newText);
+        diff.value = newText.charAt(diff.idx);
+        break;
     }
+    return diff;
+  }
 
-    getDiffIdx(prevText: string, newText: string): number {
-        let idx: number = 0;
-        while (prevText.charAt(idx) == newText.charAt(idx)) {
-            idx++;
-            if (idx > Math.max(prevText.length, newText.length)) break;
-        }
-        return idx;
+  getDiffIdx(prevText: string, newText: string): number {
+    let idx = 0;
+    while (prevText.charAt(idx) == newText.charAt(idx)) {
+      idx++;
+      if (idx > Math.max(prevText.length, newText.length)) break;
     }
+    return idx;
+  }
 }
