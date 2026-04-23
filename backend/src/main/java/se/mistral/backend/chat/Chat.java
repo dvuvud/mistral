fd676501cc2bd79f8358c6f942fdf92b92e4ba46
Alec <alec@andin.se>
2026-04-23 14:49:55 +0200

    ADD chat entity

--- backend/src/main/java/se/mistral/backend/chat/Chat.java
+++ backend/src/main/java/se/mistral/backend/chat/Chat.java
@@ -1,4 +1,40 @@
 package se.mistral.backend.chat;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "chat")
 public class Chat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private int sender;
+
+    @Column(nullable = false)
+    private int recipient;
+
+    @Column(name = "timestamp", nullable = false)
+    private LocalDateTime timestamp;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String message;
+
 }
