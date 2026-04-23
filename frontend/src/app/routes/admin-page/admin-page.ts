405e50e44b6d318005a6d9423f2098bf1e28d339
Imran Abdihakim <imuu.dv@outlook.com>
2026-04-23 03:20:25 +0200

    fix child-list not loading to admin page

--- frontend/src/app/routes/admin-page/admin-page.ts
+++ frontend/src/app/routes/admin-page/admin-page.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatCard, MatCardHeader, MatCardContent, MatCardModule } from "@angular/material/card";
 import { AddChildForm } from './add-child-form/add-child-form';
 import { ChildAdminList } from './child-admin-list/child-admin-list';
 import { CommonModule } from '@angular/common';
 import { Group } from './group/group';
+import { MatToolbar } from '@angular/material/toolbar';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'admin-page',
@@ -15,10 +18,19 @@ import { Group } from './group/group';
     AddChildForm,
     ChildAdminList,
     Group,
-    CommonModule
+    CommonModule,
+    MatToolbar
   ],
   templateUrl: './admin-page.html',
   styleUrl: './admin-page.scss',
 })
 export class AdminPage {
+  
+  private router = inject(Router);
+  logout() {
+    document.cookie = 'jwtToken=""';
+    localStorage.removeItem('token');
+    this.router.navigateByUrl('/');
+  }
 }
+
