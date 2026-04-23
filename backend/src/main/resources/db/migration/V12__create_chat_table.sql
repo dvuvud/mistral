49ecff7a481d73041b2ff9cbf3afdc397b6774d2
Alec <alec@andin.se>
2026-04-23 14:30:18 +0200

    FIX add timestamp to chat_table

--- backend/src/main/resources/db/migration/V12__create_chat_table.sql
+++ backend/src/main/resources/db/migration/V12__create_chat_table.sql
@@ -3,5 +3,6 @@ CREATE TABLE chat
     id BIGSERIAL PRIMARY KEY,
     sender INT NOT NULL,
     recipient INT NOT NULL,
-    message TEXT NOT NULL DEFAULT ''
+    message TEXT NOT NULL DEFAULT '',
+    timestamp TIMESTAMP NOT NULL
 );
