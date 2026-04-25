4f0ff53e09aa28c464ea541bdbbde5098cc68f61
Alec <alec@andin.se>
2026-04-26 01:55:21 +0200

    FIX use ChatMessage instead of Message to avoid confusion other Message types

--- backend/src/main/resources/db/migration/V12__create_chat_table.sql
+++ backend/src/main/resources/db/migration/V12__create_chat_table.sql
@@ -3,6 +3,6 @@ CREATE TABLE chat
     id BIGSERIAL PRIMARY KEY,
     sender BIGINT NOT NULL,
     recipient BIGINT NOT NULL,
-    message TEXT NOT NULL DEFAULT '',
+    chatMessage TEXT NOT NULL DEFAULT '',
     timestamp TIMESTAMP NOT NULL
 );
