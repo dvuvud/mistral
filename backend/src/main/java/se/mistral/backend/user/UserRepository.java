eb84ace739df9fa414cbb0023ba4a3b24960fb41
dvuvud <carldavid.buhrman@gmail.com>
2026-04-26 20:05:26 +0200

    clean up query

--- backend/src/main/java/se/mistral/backend/user/UserRepository.java
+++ backend/src/main/java/se/mistral/backend/user/UserRepository.java
@@ -3,12 +3,15 @@ package se.mistral.backend.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import se.mistral.backend.user.dto.BasicUserInformation;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
-    @Query("select user from User user where user.role = TEACHER")
-    List<User> findUserByRole_Teacher();
+    @Query("SELECT new se.mistral.backend.user.dto.BasicUserInformation(user.id, user.name) " + 
+           "FROM User user WHERE user.role = TEACHER")
+    List<BasicUserInformation> findUserByRole_Teacher();
 }
