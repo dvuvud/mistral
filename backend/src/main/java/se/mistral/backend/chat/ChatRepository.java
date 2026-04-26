582cd60f2507115062100e697bd1dff076037f18
Alec <alec@andin.se>
2026-04-26 02:14:43 +0200

    ADD retrieve history support in ChatRepository

--- backend/src/main/java/se/mistral/backend/chat/ChatRepository.java
+++ backend/src/main/java/se/mistral/backend/chat/ChatRepository.java
@@ -1,4 +1,12 @@
 package se.mistral.backend.chat;
 
-public interface ChatRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import se.mistral.backend.chat.dto.ChatMessage;
+
+import java.util.List;
+
+@Repository
+public interface ChatRepository extends JpaRepository<Chat, Long>{
+    List<ChatMessage> findAllMessageWithSenderIdAndRecipientId(Long senderId, Long recipientId);
 }
