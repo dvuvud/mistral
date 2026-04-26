b4e00c0459d7142c4da6d21d3bfd023178cdb953
dvuvud <carldavid.buhrman@gmail.com>
2026-04-26 20:58:23 +0200

    fix retrieve chat history endpoint to use query params instead of request body

--- backend/src/main/java/se/mistral/backend/chat/ChatController.java
+++ backend/src/main/java/se/mistral/backend/chat/ChatController.java
@@ -1,14 +1,12 @@
 package se.mistral.backend.chat;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import se.mistral.backend.chat.dto.ChatMessage;
-import se.mistral.backend.chat.dto.RetrieveHistoryRequest;
 
 import java.util.List;
 
@@ -20,8 +18,8 @@ public class ChatController {
     private final ChatService chatService;
 
     @GetMapping("/history")
-    public ResponseEntity<List<ChatMessage>> retrieveHistory(@Valid @RequestBody RetrieveHistoryRequest retrieveHistoryRequest) {
-        return ResponseEntity.ok(chatService.retrieveHistory(retrieveHistoryRequest));
+    public ResponseEntity<List<ChatMessage>> retrieveHistory(@RequestParam long senderId, @RequestParam long recipientId) {
+        return ResponseEntity.ok(chatService.retrieveHistory(senderId, recipientId));
     }
 
 }
