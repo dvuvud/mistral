122c484fd182d6a5b7678dedd1fbf468413390b6
Alec <alec@andin.se>
2026-04-26 16:50:32 +0200

    FIX simplified retriveHistory as logic was moved to query

--- backend/src/main/java/se/mistral/backend/chat/ChatService.java
+++ backend/src/main/java/se/mistral/backend/chat/ChatService.java
@@ -5,10 +5,7 @@ import org.springframework.stereotype.Service;
 import se.mistral.backend.chat.dto.ChatMessage;
 import se.mistral.backend.chat.dto.RetrieveHistoryRequest;
 
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Stream;
-
 
 @RequiredArgsConstructor
 @Service
@@ -27,18 +24,10 @@ public class ChatService {
     }
 
     public List<ChatMessage> retrieveHistory(RetrieveHistoryRequest retrieveHistoryRequest) {
-        List<ChatMessage> sentHistory = chatRepository.findBySenderAndRecipient(
+        return chatRepository.findConversationHistory(
                 retrieveHistoryRequest.senderId(),
-                retrieveHistoryRequest.recipientId());
-
-        List<ChatMessage> receivedHistory = chatRepository.findBySenderAndRecipient(
-                retrieveHistoryRequest.recipientId(),
-                retrieveHistoryRequest.senderId());
-
-        List<ChatMessage> fullHistory = new java.util.ArrayList<>(Stream.concat(sentHistory.stream(), receivedHistory.stream()).toList());
-        fullHistory.sort(Comparator.comparing(ChatMessage::timestamp));
-
-        return fullHistory;
+                retrieveHistoryRequest.recipientId()
+        );
     }
 
 }
