c260e801fe2a2857bbefdad1f2f3851ab47f6490
Alec <alec@andin.se>
2026-04-26 16:49:43 +0200

    FIX made a better query to retrieve history

--- backend/src/main/java/se/mistral/backend/chat/ChatRepository.java
+++ backend/src/main/java/se/mistral/backend/chat/ChatRepository.java
@@ -9,6 +9,11 @@ import java.util.List;
 
 @Repository
 public interface ChatRepository extends JpaRepository<Chat, Long> {
-    @Query("SELECT chat FROM Chat chat WHERE chat.senderId = :senderId AND chat.recipientId = :recipientId")
-    List<ChatMessage> findBySenderAndRecipient(Long senderId, Long recipientId);
+    @Query("""
+    SELECT chat FROM Chat chat
+    WHERE (chat.senderId = :user1 AND chat.recipientId = :user2)
+       OR (chat.senderId = :user2 AND chat.recipientId = :user1)
+    ORDER BY chat.timestamp ASC
+""")
+    List<ChatMessage> findConversationHistory(Long senderId, Long recipientId);
 }
