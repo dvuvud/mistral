abb555c48d8bb3659ee019f51fde5182a1866226
Imran Abdihakim <imuu.dv@outlook.com>
2026-04-27 01:50:20 +0200

    add first/last name fields with group selection, and deleting a child

--- frontend/src/app/core/child/child.service.ts
+++ frontend/src/app/core/child/child.service.ts
@@ -2,12 +2,22 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
+import { Group } from '../../routes/admin-page/group/group';
 
 export interface Child {
   id: number;
   name: string;
   date: string | null;
   present: boolean | null;
+  group?: {
+    id: number;
+    name: string;
+  } | null;
+}
+
+export interface GroupData {
+  id: number;
+  name: string;
 }
 
 @Injectable({ providedIn: 'root' })
@@ -25,20 +35,30 @@ export class ChildService {
   }
 
   getAll(): Observable<Child[]> {
-    return this.http.get<Child[]>(this.childUrl);
+    return this.http.get<Child[]>(`${this.adminUrl}/children`);
   }
 
-  createChild(name: string): Observable<any> {
-    return this.http.post(`${this.adminUrl}/child`, { name });
+  createChild(name: string): Observable<Child> {
+    return this.http.post<Child>(`${this.adminUrl}/child`, { name });
   }
 
   createGroup(name: string): Observable<any> {
     return this.http.post(`${this.adminUrl}/group`, { name });
   }
 
+  deleteChild(id: number): Observable<any> {
+    return this.http.delete(`${this.adminUrl}/child/${id}`);
+  }
+
+  getGroups(): Observable<GroupData[]> {
+    return this.http.get<GroupData[]>(`${this.adminUrl}/groups`);
+  }
+
 
   //TODO
-  assignChildToGroup(groupId: string, childId: string) { }
+  assignChildToGroup(groupId: string, childId: number): Observable<any> {
+    return this.http.put(`${this.adminUrl}/group/${groupId}/child/${childId}`, {});
+  }
 
   //TODO
   activateUser() { }
--- frontend/src/app/routes/admin-page/group/group.ts
+++ frontend/src/app/routes/admin-page/group/group.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { ChildService, GroupData } from '../../../core/child/child.service';
 @Component({
   selector: 'group',
-  imports: [],
+  imports: [
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+
+
+  ],
   templateUrl: './group.html',
   styleUrl: './group.scss',
 })
-export class Group {
+export class Group implements OnInit {
+
+  private childService = inject(ChildService);
+
+
+  groups: GroupData[] = [];
+
+  ngOnInit(): void {
+
+    this.childService.getGroups().subscribe({
+      next: (groups) => this.groups = groups,
+      error: (err) => console.error(err)
+    });
+
+  }
 
 }
