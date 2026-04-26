d49a1e41972aa808932d6a837ff8b5143e6a10dc
Alec <alec@andin.se>
2026-04-26 05:32:19 +0200

    FIX made custom SQL so it actually works can maybe be generated but this definitely works.

--- backend/src/main/java/se/mistral/backend/chat/ChatRepository.java
+++ backend/src/main/java/se/mistral/backend/chat/ChatRepository.java
@@ -1,12 +1,14 @@
 package se.mistral.backend.chat;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import se.mistral.backend.chat.dto.ChatMessage;
 
 import java.util.List;
 
 @Repository
-public interface ChatRepository extends JpaRepository<Chat, Long>{
-    List<ChatMessage> findAllMessageWithSenderIdAndRecipientId(Long senderId, Long recipientId);
+public interface ChatRepository extends JpaRepository<Chat, Long> {
+    @Query("SELECT chat FROM Chat chat WHERE chat.senderId = :senderId AND chat.recipientId = :recipientId")
+    List<ChatMessage> findBySenderAndRecipient(Long senderId, Long recipientId);
 }
