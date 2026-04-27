29f98ac7485efadb003cf0d0a04c220c12e50e1c
dvuvud <carldavid.buhrman@gmail.com>
2026-04-27 13:06:38 +0200

    update journal entity to match table

--- backend/src/main/java/se/mistral/backend/journal/Journal.java
+++ backend/src/main/java/se/mistral/backend/journal/Journal.java
@@ -6,8 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
 import jakarta.persistence.Version;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,26 +15,33 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+import org.hibernate.annotations.Check;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "journals", uniqueConstraints = {@UniqueConstraint(name = "UniqueChildIdAndDate", columnNames = {"child_id", "date"})
-})
+@Table(name = "journals")
+@Check(constraints = "(child_id IS NOT NULL AND group_id IS NULL) OR (child_id IS NULL AND group_id IS NOT NULL)")
 public class Journal {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "child_id", nullable = false)
+    @Column(name = "child_id")
     private Long childId;
 
+    @Column(name = "group_id")
+    private Long groupId;
+
     @Column(nullable = false)
+    @NotNull
     private LocalDate date;
 
     @Column(nullable = false, columnDefinition = "TEXT")
+    @NotNull
     private String content;
 
     @Version
