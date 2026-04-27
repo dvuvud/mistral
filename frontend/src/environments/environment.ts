2c7bfb48f6682fe9b1ab5aba8169adde0a6ebb7b
dvuvud <carldavid.buhrman@gmail.com>
2026-04-27 09:08:52 +0200

    fix cors config to support credentials

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,8 @@
 export const environment = {
-  apiUrl: 'http://localhost:8080', // ändra till https med rätt url när vi testar med ngrok
-  wsUrl: 'ws://localhost:8080'     // ändra till wss med rätt url när vi testar med ngrok
+  apiUrl: 'http://localhost:8080',
+  wsUrl: 'ws://localhost:8080'
+  // Använd följande i samband med ngrok och kommentera ut dem över
+  // apiUrl: 'https://salad-dramatize-customary.ngrok-free.dev',
+  // wsUrl: 'wss://salad-dramatize-customary.ngrok-free.dev'
 };
 
