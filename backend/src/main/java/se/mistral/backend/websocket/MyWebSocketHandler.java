524d0c1355630b70afea6010df0f97b53a8c97d5
dvuvud <carldavid.buhrman@gmail.com>
2026-04-28 22:32:54 +0200

    remove unused import

--- backend/src/main/java/se/mistral/backend/websocket/MyWebSocketHandler.java
+++ backend/src/main/java/se/mistral/backend/websocket/MyWebSocketHandler.java
@@ -2,7 +2,6 @@ package se.mistral.backend.websocket;
 
 import java.io.IOException;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
