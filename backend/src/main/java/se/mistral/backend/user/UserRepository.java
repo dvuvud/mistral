70a28b17eeff51e00552a8ce3ea2527abedf9abe
Alec <alec@andin.se>
2026-05-04 20:17:04 +0200

    ADD User documentation

--- backend/src/main/java/se/mistral/backend/user/UserRepository.java
+++ backend/src/main/java/se/mistral/backend/user/UserRepository.java
@@ -9,9 +9,28 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    /**
+     * Find user by email if email exists.
+     *
+     * @param email the email
+     * @return the optional user
+     */
     Optional<User> findByEmail(String email);
+
+    /**
+     * Check if the email exists in the database.
+     *
+     * @param email the email
+     * @return the boolean if the email exists or not.
+     */
     boolean existsByEmail(String email);
-    @Query("SELECT new se.mistral.backend.user.dto.BasicUserInformation(user.id, user.name) " + 
+
+    /**
+     * Find user by role teacher.
+     *
+     * @return the list of all teachers
+     */
+    @Query("SELECT new se.mistral.backend.user.dto.BasicUserInformation(user.id, user.name) " +
            "FROM User user WHERE user.role = TEACHER")
     List<BasicUserInformation> findUserByRole_Teacher();
 }
--- backend/src/main/java/se/mistral/backend/user/UserService.java
+++ backend/src/main/java/se/mistral/backend/user/UserService.java
@@ -7,17 +7,27 @@ import se.mistral.backend.user.dto.BasicUserInformation;
 import java.util.List;
 import java.util.Optional;
 
-
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
 
+    /**
+     * Find user by email.
+     *
+     * @param email the email
+     * @return the optional user if they exist
+     */
     public Optional<User> findUserByEmail(String email) {
         return userRepository.findByEmail(email);
     }
 
+    /**
+     * Retrieve all teachers.
+     *
+     * @return the list of all teachers.
+     */
     public List<BasicUserInformation> retrieveAllTeachers() {
         return userRepository.findUserByRole_Teacher();
     }
