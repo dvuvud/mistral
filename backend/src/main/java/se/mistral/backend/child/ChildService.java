949cbdf7ff9fd8f20ac0edec6432427da830746f
Alec <alec@andin.se>
2026-05-04 20:15:53 +0200

    ADD Child documentation

--- backend/src/main/java/se/mistral/backend/child/ChildService.java
+++ backend/src/main/java/se/mistral/backend/child/ChildService.java
@@ -18,6 +18,12 @@ public class ChildService {
 
     private final ChildRepository childRepository;
 
+    /**
+     * Create a child.
+     *
+     * @param request the request
+     * @return the created child
+     */
     public ChildResponse createChild(CreateChildRequest request) {
         Child child = new Child();
         child.setName(request.name());
@@ -25,26 +31,60 @@ public class ChildService {
         return new ChildResponse(saved.getId(), saved.getName());
     }
 
+    /**
+     * Gets all children.
+     *
+     * @return all children
+     */
     public List<ChildResponse> getAllChildren() {
         return childRepository.findAllIdsAndNames();
     }
 
+    /**
+     * Gets all children on a given date
+     *
+     * @param date the date
+     * @return the attendance list of all children registered on the date.
+     */
     public List<AttendanceResponse> getAllChildrenWithAttendanceIfExists(LocalDate date) {
         return childRepository.findAllChildrenWithAttendanceIfExists(date);
     }
 
+    /**
+     * Gets all children on a given date in a group.
+     *
+     * @param date    the date
+     * @param groupId the group id
+     * @return the attendance list of all children registered on the date in the group.
+     */
     public List<AttendanceResponse> getAllChildrenWithAttendanceIfExistsInGroup(LocalDate date, Long groupId) {
         return childRepository.findAllChildrenWithAttendanceIfExistsInGroup(date, groupId);
     }
 
+    /**
+     * Gets children by group.
+     *
+     * @param groupId the group id
+     * @return the list of all children in the group
+     */
     public List<ChildResponse> getChildrenByGroup(Long groupId) {
         return childRepository.findAllByGroupId(groupId);
     }
 
+    /**
+     * Gets all children by group.
+     *
+     * @return the list of all children in the group, with group information
+     */
     public List<ChildWithGroupResponse> getAllChildrenWithGroup() {
         return childRepository.findAllWithGroup();
     }
 
+    /**
+     * Delete child.
+     *
+     * @param id the id
+     */
     public void deleteChild(Long id) {
         if (!childRepository.existsById(id)) {
             throw new NotFoundException("Child not found");
