73fb6198c5894349691a55f4f4a105e4a8676b5b
Alec <alec@andin.se>
2026-05-04 20:15:08 +0200

    ADD Attendance documentation

--- backend/src/main/java/se/mistral/backend/attendance/AttendanceService.java
+++ backend/src/main/java/se/mistral/backend/attendance/AttendanceService.java
@@ -10,6 +10,7 @@ import se.mistral.backend.attendance.dto.AttendanceDto;
 
 import java.time.LocalDate;
 
+
 @Service
 @RequiredArgsConstructor
 public class AttendanceService {
@@ -17,11 +18,24 @@ public class AttendanceService {
     private final AttendanceRepository attendanceRepository;
     private final ChildRepository childRepository;
 
+    /**
+     * Gets attendance of a child on a given date.
+     *
+     * @param childId the child id
+     * @param date    the date
+     * @return the attendance dto of the child at the given date
+     */
     public AttendanceDto getAttendance(Long childId, LocalDate date) {
         return attendanceRepository.findPresenceByChildIdAndDate(childId, date)
         .orElseThrow(() -> new NotFoundException("Attendance is not logged for this child on " + date.toString()));
     }
 
+    /**
+     * Updates the attendance of a child on a given date from the request.
+     *
+     * @param request the request
+     * @return the attendance dto of the new status of the attendance for the specified child
+     */
     public AttendanceDto updateAttendance(AttendanceRequest request) {
         Attendance attendance = attendanceRepository.findByChildIdAndDate(request.childId(), request.date())
             .orElseGet(() -> Attendance.builder()
