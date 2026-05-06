05dd608cbf2f77576bdbb245ca28713e0c5ec9e8
Ivan <ivan.brolin.5150@student.uu.se>
2026-05-06 15:14:05 +0200

    add fungerande 'replacement'

--- frontend/src/app/routes/main-page/main-live-journal/text-diff.ts
+++ frontend/src/app/routes/main-page/main-live-journal/text-diff.ts
@@ -1,22 +1,71 @@
 export interface Diff {
-    operation: 'INSERT' | 'DELETE';
-    idx: number;
-    value: string;
+    operation: 'INSERT' | 'DELETE' | 'REPLACEMENT';
+    idx: number; // position av cursor efter förändring
+    value: string; // används för insert delen
+    length: number // används för delete delen
 }
 
 export class textDiff {
   getDiff(prevText: string, newText: string, idx: number): Diff {
-    const length = prevText.length - newText.length
-    const operation = length < 0 ? 'INSERT' : 'DELETE';
-    const diff: Diff = {operation: operation, idx: idx, value: ''}
+    const length = prevText.length - newText.length;
+
+    const operation = this.surroundingMatch(prevText, newText, idx, length) 
+    ? length > 0 
+    ? 'DELETE' 
+    : 'INSERT' 
+    : 'REPLACEMENT'
+
+    const diff: Diff = {operation: operation, idx: idx, value: '', length: 0}
+
     switch (operation) {
       case 'DELETE':
-        diff.value = length.toString(); // lite ineffektivt att förvandla fram och tillbaka men blir smidigt att koda
+        diff.length = length; // lite ineffektivt att förvandla fram och tillbaka men blir smidigt att koda
         break;
       case 'INSERT':
+        diff.idx = idx + length;
         diff.value = newText.substring(idx + length, idx); // blir minus eftersom att insert ger negativ "length"
         break;
+      case 'REPLACEMENT':
+        diff.value = newText.substring(this.findFirstDiff(prevText, newText), this.findLastDiff(prevText, newText)); 
+        diff.length = length + diff.value.length;
+        diff.idx = idx - diff.value.length;
+        break;
     }
+    console.log(diff);
     return diff;
+  }
+
+  // kollar om allt runt längdskillnaden matchar
+  surroundingMatch(prevText: string, newText: string, idx: number, length: number): boolean {
+    switch (length < 0) {
+      case true:
+        return (prevText.substring(0, idx + length) === newText.substring(0, idx + length)) 
+        && (prevText.substring(idx + length) === newText.substring(idx))
+      case false:
+        return (prevText.substring(0, idx) === newText.substring(0, idx)) 
+        && (prevText.substring(idx + length) === newText.substring(idx))
+    }
+  }
+
+  findFirstDiff(prevText: string, newText: string): number {
+    let idx = 0;
+    while(true) {
+      if (prevText.charAt(idx) === newText.charAt(idx)) {
+        idx++;
+      } else {
+        return idx;
+      }
+    }
+  }
+
+  findLastDiff(prevText: string, newText: string): number {
+    let idx = 1;
+    while(true) {
+      if (prevText.charAt(prevText.length - idx) === newText.charAt(newText.length - idx)) {
+        idx++;
+      } else {
+        return newText.length - idx + 1;
+      }
+    }
   }
 }
