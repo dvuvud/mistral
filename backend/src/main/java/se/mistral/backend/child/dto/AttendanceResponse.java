201e5207dc6959c4d5ac9a33cb3218cfa4be0092
MuschBenne <sjostrand.benjamin@gmail.com>
2026-05-06 12:58:57 +0200

    Update child attendance queries to use AttendanceStatus

--- backend/src/main/java/se/mistral/backend/child/dto/AttendanceResponse.java
+++ backend/src/main/java/se/mistral/backend/child/dto/AttendanceResponse.java
@@ -1,10 +1,12 @@
 package se.mistral.backend.child.dto;
 
+import se.mistral.backend.attendance.AttendanceStatus;
+
 import java.time.LocalDate;
 
 public record AttendanceResponse(
     Long id,
     String name,
     LocalDate date,
-    Boolean present
+    AttendanceStatus status
 ) { }
