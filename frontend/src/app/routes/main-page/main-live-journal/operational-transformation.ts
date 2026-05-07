d2970369d6be44e77a7f949ba4fd24da60c6f4f4
Ivan <ivan.brolin.5150@student.uu.se>
2026-05-07 11:48:05 +0200

    add OT justerad för att hantera längre stycken

--- frontend/src/app/routes/main-page/main-live-journal/operational-transformation.ts
+++ frontend/src/app/routes/main-page/main-live-journal/operational-transformation.ts
@@ -1,7 +1,8 @@
-import { WsJournalWriteOperation } from "../../../core/websocket/websocket.service";
+import { DeleteOperation, WsJournalWriteOperation } from "../../../core/websocket/websocket.service";
 
 export class operationalTransformation {
   transformClient(inOp: WsJournalWriteOperation, flightOp: WsJournalWriteOperation): WsJournalWriteOperation {
+    // we transform based on type of incoming operation
     switch (inOp.type) {
       case 'INSERT':
         return this.transformInsert(inOp, flightOp);
@@ -14,13 +15,15 @@ export class operationalTransformation {
     const resOp = inOp;
     switch (flightOp.type) {
       case 'INSERT':
-        if (flightOp.position < inOp.position) {
-          resOp.position++;
+        if (flightOp.position <= inOp.position) {
+          // if flightOp is before or at incoming insert, shift incoming to the right    
+          resOp.position += flightOp.text.length;
         }
         break;
       case 'DELETE':
         if (flightOp.position < inOp.position) {
-          resOp.position--;
+          // if flightOp is before incoming delete 
+          Math.max(flightOp.position, resOp.position -= flightOp.length);
         }
         break;
     }
@@ -28,16 +31,28 @@ export class operationalTransformation {
   }
 
   transformDelete(inOp: WsJournalWriteOperation, flightOp: WsJournalWriteOperation): WsJournalWriteOperation {
-    const resOp = inOp;
+    const resOp = inOp as DeleteOperation;
     switch (flightOp.type) {
       case 'INSERT':
-        if (flightOp.position < inOp.position) {
-          resOp.position++;
+        if (flightOp.position <= resOp.position) {
+          // flightOp is before or at the start of the incoming delete operation, shift incoming right
+          resOp.position += flightOp.text.length;
+        } else if (flightOp.position < resOp.position + resOp.length) {
+          // flightOp starts after the start of the incoming delete operation but after it's end,
+          // extend the range of the incoming operation
+          resOp.length += flightOp.text.length;
         }
         break;
       case 'DELETE':
-        if (flightOp.position < inOp.position) {
-          resOp.position--;
+        if (flightOp.position + flightOp.length <= resOp.position) {
+          // the entirety of the flightOp delete is prior to the starting position of the incoming operation,
+          // shift the incoming operation left, but no further than the start of the flightOp
+          resOp.position -= flightOp.length;
+        } else if (flightOp.position <= resOp.position) {
+          // there is crossover between flightOp and resOp, adjustment needed
+          const crossOverLength = flightOp.position + flightOp.length - resOp.position;
+          resOp.position = Math.max(flightOp.position, resOp.position -= flightOp.length);
+          resOp.length -= crossOverLength;
         }
         break;
     }
