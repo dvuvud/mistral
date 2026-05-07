cc270244c309ee6a001e67a03c900478b0ff0a9d
dvuvud <carldavid.buhrman@gmail.com>
2026-05-07 05:25:50 +0200

    fix odd formatting with jldts buffer-formatter

--- backend/src/main/java/se/mistral/backend/user/dto/UpdateColorRequest.java
+++ backend/src/main/java/se/mistral/backend/user/dto/UpdateColorRequest.java
@@ -4,5 +4,5 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record UpdateColorRequest(
-    @NotBlank @Pattern(regexp = "^#[0-9A-Fa-f]{6}$") String color
-) { }
+        @NotBlank @Pattern(regexp = "^#[0-9A-Fa-f]{6}$") String color) {
+}
