4118fffdb20f4ef4b9e65dce89a47a27879bdeae
MuschBenne <sjostrand.benjamin@gmail.com>
2026-05-08 16:07:13 +0200

    update Child interface to use status instead of present

--- frontend/src/app/core/child/child.service.ts
+++ frontend/src/app/core/child/child.service.ts
@@ -2,12 +2,13 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
+import { AttendanceStatus } from './attendance.service';
 
 export interface Child {
   id: number;
   name: string;
   date: string | null;
-  present: boolean | null;
+  status: AttendanceStatus | null;
 }
 
 @Injectable({ providedIn: 'root' })
