4704f63fcd9feb61da5a5af871d4fe69a0576bca
Hiba Ahmad  hiah5302 <hiba.ahmad.5302@student.uu.se>
2026-05-11 18:10:06 +0200

    sista ändringar för tillfälligt

--- frontend/src/app/routes/main-page/main-chat/main-chat.ts
+++ frontend/src/app/routes/main-page/main-chat/main-chat.ts
@@ -1,7 +1,8 @@
-import { Component, inject, signal } from "@angular/core";
+import { Component, inject, OnDestroy, OnInit, signal } from "@angular/core";
 import { ChatService, ChatMessage } from "../../../core/chat/chat.service";
 import { WebsocketService, WsChatMessage, WsMailbox } from "../../../core/websocket/websocket.service"; 
 import { userService, User } from "../../../core/user/user.service";
+import { Subscription } from "rxjs";
 
 @Component({
     selector: 'main-chat',
@@ -9,15 +10,32 @@ import { userService, User } from "../../../core/user/user.service";
     styleUrl: './main-chat.scss'
 })
 
-export class MainChat {
-    currentUserId = Number(localStorage.getItem('UserId'));
+export class MainChat implements OnInit, OnDestroy {
+
+    userId = Number(localStorage.getItem('UserId'));
     users= signal<User[]>([]);
     selectedTeacher = signal<User | null> (null);
     messages = signal<ChatMessage[]>([]);
     newMessage = signal<string>('');
+  // Samlar alla subscriptions för enkel cleanup i ngOnDestroy
+    private subs = new Subscription();
 
     private chatService = inject(ChatService);
     private websocketService = inject(WebsocketService);
     private userService = inject(userService);
 
+   async ngOnInit(): Promise<void> {
+    await this.websocketService.ensureConnected();
+    // load all teachers for connect list
+    this.subs.add(
+      this.userService.getUsers().subscribe(users => {
+        this.users.set(users);
+      })
+    );
+    this
+  }
+  ngOnDestroy(): void {
+    // Rensa alla subscriptions när komponenten förstörs
+    this.subs.unsubscribe();
+  }
 }
