448c35e2108a930dd6fcaf8462d5c346ece04177
Ivan <ivan.brolin.5150@student.uu.se>
2026-05-11 11:47:02 +0200

    add tests for replace & edge cases

--- frontend/src/app/routes/main-page/main-live-journal/text-diff.test.ts
+++ frontend/src/app/routes/main-page/main-live-journal/text-diff.test.ts
@@ -198,8 +198,143 @@ describe('textDiff', () => {
     });
   });
 
-  describe('getDiff - REPLACE', () => {
+  describe('getDiff - REPLACEMENT', () => {
 
-    
+    describe('REPLACE - single char', () => {
+
+      it('should detect a single-char replace at the end', () => {
+        const prevText = 'hello';
+        const newText = 'hellA';
+        const idx = 5; 
+
+        const diff = differ.getDiff(prevText, newText, idx);
+
+        expect(diff.operation).toBe('REPLACEMENT');
+        expect(diff.idx).toBe(4);
+        expect(diff.value).toBe('A');
+        expect(diff.length).toBe(1);
+      });
+
+      it('should detect a single-char replace in the middle', () => {
+        const prevText = 'hello';
+        const newText = 'heWlo';
+        const idx = 3;
+
+        const diff = differ.getDiff(prevText, newText, idx);
+
+        expect(diff.operation).toBe('REPLACEMENT');
+        expect(diff.idx).toBe(2);
+        expect(diff.value).toBe('W');
+        expect(diff.length).toBe(1);
+      });
+
+      it('should detect a single-char replace at the beginning', () => {
+        const prevText = 'hello';
+        const newText = 'Cello';
+        const idx = 1;
+
+        const diff = differ.getDiff(prevText, newText, idx);
+
+        expect(diff.operation).toBe('REPLACEMENT');
+        expect(diff.idx).toBe(0);
+        expect(diff.value).toBe('C');
+        expect(diff.length).toBe(1);
+      });
+
+      it('should detect a single-char replace on final char', () => {
+        const prevText = 'h';
+        const newText = 'a';
+        const idx = 1;
+
+        const diff = differ.getDiff(prevText, newText, idx);
+
+        expect(diff.operation).toBe('REPLACEMENT');
+        expect(diff.idx).toBe(0);
+        expect(diff.value).toBe('a');
+        expect(diff.length).toBe(1);
+      });
+
+    });
+
+    describe('REPLACE - multiple char', () => {
+
+      it('should detect a multi-char replace at the end', () => {
+        const prevText = 'hello';
+        const newText = 'helAB';
+        const idx = 5;
+
+        const diff = differ.getDiff(prevText, newText, idx);
+
+        expect(diff.operation).toBe('REPLACEMENT');
+        expect(diff.idx).toBe(3);
+        expect(diff.value).toBe('AB');
+        expect(diff.length).toBe(2);
+      });
+      
+      it('should detect a multi-char replace in the middle', () => {
+        const prevText = 'hello';
+        const newText = 'hABCo';
+        const idx = 4;
+        
+        const diff = differ.getDiff(prevText, newText, idx);
+        
+        expect(diff.operation).toBe('REPLACEMENT');
+        expect(diff.idx).toBe(1);
+        expect(diff.value).toBe('ABC');
+        expect(diff.length).toBe(3);
+      });
+
+      it('should detect a multi-char replace at the begining', () => {
+        const prevText = 'hello';
+        const newText = 'ABllo';
+        const idx = 2;
+        
+        const diff = differ.getDiff(prevText, newText, idx);
+        
+        expect(diff.operation).toBe('REPLACEMENT');
+        expect(diff.idx).toBe(0);
+        expect(diff.value).toBe('AB');
+        expect(diff.length).toBe(2);
+      });
+
+      it('should detect a multi-char replace on entire text', () => {
+        const prevText = 'hello';
+        const newText = 'ABCDE';
+        const idx = 5;
+        
+        const diff = differ.getDiff(prevText, newText, idx);
+        
+        expect(diff.operation).toBe('REPLACEMENT');
+        expect(diff.idx).toBe(0);
+        expect(diff.value).toBe('ABCDE');
+        expect(diff.length).toBe(5);
+      });
+    });
+
+    describe('edge cases', () => {
+      it('should pivot to insert', () => {
+        const prevText = 'hello';
+        const newText = 'hehello';
+        const idx = 6; // index är 6 eftersom vi böt ut "hell" mot "hehell"
+        
+        const diff = differ.getDiff(prevText, newText, idx);
+        
+        expect(diff.operation).toBe('INSERT');
+        expect(diff.idx).toBe(0);
+        expect(diff.value).toBe('he');
+      });
+
+      it('should pivot to delete', () => {
+        const prevText = 'hehello';
+        const newText = 'hello';
+        const idx = 4; // index är 4 eftersom vi böt ut "hehell" mot "hell"
+        
+        const diff = differ.getDiff(prevText, newText, idx);
+        
+        expect(diff.operation).toBe('DELETE');
+        expect(diff.idx).toBe(0);
+        expect(diff.length).toBe(2);
+      });
+    })
   }); 
 });
