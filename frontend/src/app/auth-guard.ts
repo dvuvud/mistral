0f35d40e5c9e4cbfa9e772313606a2b70dcc3a2a
dvuvud <carldavid.buhrman@gmail.com>
2026-05-11 17:29:14 +0200

    replace isAuthorized with synchronous getRole using role stored in session storage after login resulting in fewer api requests, and stuff, I am writing a very long message here, wow

--- frontend/src/app/auth-guard.ts
+++ frontend/src/app/auth-guard.ts
@@ -2,18 +2,22 @@ import { ActivatedRouteSnapshot, CanActivateFn, Router } from '@angular/router';
 import { AuthService } from './core/auth/auth.service';
 import { inject } from '@angular/core';
 
-export const authGuard: CanActivateFn = async (
-  route: ActivatedRouteSnapshot) => {
+export const authGuard: CanActivateFn = (route: ActivatedRouteSnapshot) => {
   const url: string = route.url.join('');
   const router = inject(Router);
   const authService = inject(AuthService);
-  const role = await authService.isAuthorized();
+  const role = authService.getRole();
 
   switch (url) {
     case '': // Redirect users already signed in from login page to /app
-      if (!role) return true;
-      if (role === 'ADMIN') return router.parseUrl('/admin');
+      if (!role) {
+        return true;
+      }
+      if (role === 'ADMIN') {
+        return router.parseUrl('/admin');
+      }
       return router.parseUrl('/app');
+
     case 'app': // Redirect non-authorized clients from /app to login page
       if (!role) {
         return router.parseUrl('/');
@@ -22,6 +26,7 @@ export const authGuard: CanActivateFn = async (
         return router.parseUrl('/admin');
       }
       return true;
+
     case 'admin':
       if (!role) {
         return router.parseUrl('/');
@@ -30,6 +35,7 @@ export const authGuard: CanActivateFn = async (
         return router.parseUrl('/app');
       }
       return true;
+
     default:
       return false;
   }
